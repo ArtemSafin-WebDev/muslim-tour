db82ac4e9a7f767a9170e358405470ce96405fd1
Артем Сафин <artemsafin.webdev@gmail.com>
2019-07-20 06:01:33 +0300

    Бегущая строка

--- src/js/modules/_ticker.js
+++ src/js/modules/_ticker.js
@@ -1,4 +1,47 @@
 
+let ticker = document.querySelector(".ticker__text");
+let initialAnimationPlayed = false;
+const animationDuration = 10;
+
+
+function setAnimationDuration() {
+    ticker.style.animationDuration = `${animationDuration}s`;
+}
+
+
+function animateNormally() {
+    ticker.addEventListener('animationend', animateNormally)
+    ticker.classList.remove('normal-animation');
+    setTimeout(function() {
+        ticker.classList.add('normal-animation');
+    }, 100)   
+}
+
+
+function setAnimation() {
+    if (!initialAnimationPlayed) {
+        ticker.classList.add('initial-animation');
+        initialAnimationPlayed = true;
+        const initialAnimationEndHandler = () => {
+            ticker.removeEventListener('animationend', initialAnimationEndHandler);
+            setAnimation();
+        }
+        ticker.addEventListener('animationend', initialAnimationEndHandler)
+    } else {
+        ticker.classList.remove('initial-animation');
+        ticker.classList.add('initially-animated');
+        animateNormally();
+    }
+}
+
+if (ticker) {
+    setAnimationDuration();
+
+    setAnimation();
+
+}
+
+
 
 
 // let viewportWidth = document.documentElement.clientWidth;
@@ -13,7 +56,7 @@
 //     var duration = tickerWidth / viewportWidth * 10;
 //     console.log('duration', duration)
 
-
+//     console.log('Показать тикер')
 //     if (initialShow) {
 //         var tickerInitialSlideWidth = tickerWidth + tickerOffsetLeft;
 //         console.log("Offset left", tickerOffsetLeft);
