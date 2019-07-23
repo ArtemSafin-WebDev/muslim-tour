552192d0584bfe75ce078b3b53e31e262d2fb3cb
Артем Сафин <artemsafin.webdev@gmail.com>
2019-07-23 22:08:47 +0300

    Переключение табов переписано на дата-атрибутах

--- src/js/modules/_select-tour.js
+++ src/js/modules/_select-tour.js
@@ -1,42 +1,40 @@
-const captionLinks = Array.prototype.slice.call(document.querySelectorAll('.page-header__caption-link'))
+const captionLinks = Array.prototype.slice.call(
+    document.querySelectorAll(".page-header__caption-link")
+);
 
 if (captionLinks && captionLinks.length >= 2) {
     let activeLink;
-    let activeTabMenuIndex;
-    const tabMenus = Array.prototype.slice.call(document.querySelectorAll('.page-header__bottom .tab-menu'))
+    let activeTab;
+    const pageHeader = document.querySelector(".page-header__bottom");
 
-    captionLinks.forEach((element, index) => {
-        if (element.classList.contains('active')) {
-            activeLink = element;
-            activeTabMenuIndex = index;
-        }
-        
-        element.addEventListener('click', function(event) {
+    function setActiveTab(event) {
+        if (this === activeLink) return;
+        if (event) {
             event.preventDefault();
-            if (this !== activeLink) {
-                activeLink.classList.remove('active');
-                activeLink = this;
-                activeLink.classList.add('active');
-                tabMenus[activeTabMenuIndex].classList.remove('active');
-                console.log('Removed from', tabMenus[activeTabMenuIndex])
-                activeTabMenuIndex = index;
-                tabMenus[activeTabMenuIndex].classList.add('active');
-                console.log('Added to', tabMenus[activeTabMenuIndex])
-            }
-        })
-    });
-
-    if (!activeLink) {
-        activeLink = captionLinks[0];
-        activeTabMenuIndex = 0;
+        }
+        const hash = this.hash.substr(1);
+        if (activeLink) {
+            activeLink.classList.remove("active");
+        }
+        activeLink = this;
+        activeLink.classList.add("active");
+        if (activeTab) {
+            activeTab.classList.remove("active");
+        }
+        const newActiveTab = pageHeader.querySelector(`[data-name='${hash}']`);
+        if (newActiveTab) {
+            activeTab = newActiveTab;
+            activeTab.classList.add('active');
+        }
     }
 
 
-    tabMenus.forEach((element, index) => {
-        if (index === activeTabMenuIndex) {
-            element.classList.add('active')
-        } else {
-            element.classList.remove('active')
-        }
-    })
+    const initialActiveTab = setActiveTab.bind(captionLinks[0]);
+    initialActiveTab();
+
+    captionLinks.forEach(link => {
+        link.addEventListener("click", setActiveTab);
+    });
+
+    
 }
