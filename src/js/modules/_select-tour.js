6ef01f7b5cd5f011f4e5b34e74c91aa465c1bac6
Артем Сафин <artemsafin.webdev@gmail.com>
2019-07-24 11:54:51 +0300

    Поправлен баг прокрутки до активного пункта таба

--- src/js/modules/_select-tour.js
+++ src/js/modules/_select-tour.js
@@ -24,16 +24,18 @@ if (captionLinks && captionLinks.length >= 2) {
         const newActiveTab = pageHeader.querySelector(`[data-name='${hash}']`);
         if (newActiveTab) {
             activeTab = newActiveTab;
-            activeTab.classList.add('active');
+            activeTab.classList.add("active");
         }
 
         moveActiveLinkToBeginning();
     }
 
     function moveActiveLinkToBeginning() {
-        activeLink.parentElement.insertBefore(activeLink, activeLink.parentElement.firstChild);
+        activeLink.parentElement.insertBefore(
+            activeLink,
+            activeLink.parentElement.firstChild
+        );
     }
-
 
     const initialActiveTab = setActiveTab.bind(captionLinks[0]);
     initialActiveTab();
@@ -42,30 +44,26 @@ if (captionLinks && captionLinks.length >= 2) {
         link.addEventListener("click", setActiveTab);
     });
 
-
-
     function moveActiveMenuItemToBeginning() {
-        const activeTabItem = document.querySelector('.tab-menu.active .tab-menu__tab--is-active');
+        const activeTabItem = document.querySelector(
+            ".tab-menu.active .tab-menu__tab--is-active"
+        );
 
-        const activeTabItemScrollingContainer = activeTabItem.parentElement.parentElement;
-        const activeTabOffsetLeft = activeTabItem.offsetLeft;
-        const paddingLeft = parseFloat(window.getComputedStyle(activeTabItem.parentElement).getPropertyValue('padding-left'))
-
-        // console.log('Scroll Width', containerScrollWidth);
-        // console.log('Offset left', activeTabOffsetLeft);
-        // console.log('Padding left', paddingLeft)
-        // console.log(activeTabOffsetLeft - paddingLeft)
-
-        activeTabItemScrollingContainer.scrollLeft = activeTabOffsetLeft - paddingLeft;
-        
+        if (activeTabItem) {
+            const activeTabItemScrollingContainer =
+                activeTabItem.parentElement.parentElement;
+            const activeTabOffsetLeft = activeTabItem.offsetLeft;
+            const paddingLeft = parseFloat(
+                window
+                    .getComputedStyle(activeTabItem.parentElement)
+                    .getPropertyValue("padding-left")
+            );
+            activeTabItemScrollingContainer.scrollLeft =
+                activeTabOffsetLeft - paddingLeft;
+        }
     }
 
     moveActiveMenuItemToBeginning();
 
-
-    const debouncedmoveActiveMenuItemToBeginning = _.debounce(moveActiveMenuItemToBeginning, 300);
-
-    window.addEventListener('resize', debouncedmoveActiveMenuItemToBeginning);
-
-    
+    window.addEventListener("resize", moveActiveMenuItemToBeginning);
 }
